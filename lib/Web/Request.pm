39d3a137834aa90350e1947b734e003986bd5d78
Jesse Luehrs <doy@tozt.net>
2013-09-02 10:41:57 -0400

    fix uri escaping (backported from Plack 5f9c372)

--- lib/Web/Request.pm
+++ lib/Web/Request.pm
@@ -112,8 +112,7 @@ has uri => (
         # in, we recognize it as /foo/bar which is not ideal, but that's
         # how the PSGI PATH_INFO spec goes and we can't do anything
         # about it. See PSGI::FAQ for details.
-        # http://github.com/miyagawa/Plack/issues#issue/118
-        my $path_escape_class = '^A-Za-z0-9\-\._~/';
+        my $path_escape_class = q{^/;:@&=A-Za-z0-9\$_.+!*'(),-};
 
         my $path = URI::Escape::uri_escape(
             $self->path_info || '',
